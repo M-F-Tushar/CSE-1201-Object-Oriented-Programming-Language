6c6ec462c10c218592ed512cebd28e285a59b774
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-06-20 08:44:28 +0600

    Add files via upload

---
+++ OOP in C++/Leacture_01: An Overview of C++/1. C++ Console I and O/3.cpp
@@ -0,0 +1,14 @@
+/*
+3. This program prompts the user for an integer value:
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i;
+    cout << " Enter a value : ";
+    cin >> i;
+    cout << " Here is your number :"  << i << endl;
+    return 0;
+}
