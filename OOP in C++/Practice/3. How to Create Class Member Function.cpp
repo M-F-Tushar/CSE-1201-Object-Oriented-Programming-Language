d70c5a2aa359ff6dfa0c2dd17c0ed5a26cbbe18d
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-06-30 09:17:04 +0600

    Add files via upload

---
+++ OOP in C++/Practice/3. How to Create Class Member Function.cpp
@@ -0,0 +1,27 @@
+/*
+Member functions are the funtions that operate on the data encapsulated in the class
+Public member funcitons are the interface to the class
+*/
+
+#include<iostream>
+using namespace std;
+
+class student
+{
+    private:
+        int roll = 10;
+    public:
+        void dispaly()
+        {
+            cout << "The roll number is : " << roll << endl;
+        }
+};
+int main()
+{
+    student s;
+
+    s.dispaly();
+
+    return 0;
+}
+
