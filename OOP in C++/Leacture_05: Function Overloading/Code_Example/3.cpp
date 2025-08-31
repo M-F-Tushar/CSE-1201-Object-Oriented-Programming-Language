e83a02013b840ef9447725cb3bd994786e05e4a2
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-08-31 23:36:00 +0600

    Add files via upload

---
+++ OOP in C++/Leacture_05: Function Overloading/Code_Example/3.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+class date
+{
+    int day, month, year;
+public:
+    date(char *str)
+    {
+        sscanf(str, "%d%*c%d%*c%d", &month, &day, &year);
+    }
+    date(int d, int m, int y)
+    {
+        day = d;
+        month = m;
+        year = y;
+    }
+
+    void show()
+    {
+        cout << "Date: " << day << "/" << month << "/" << year << endl;
+    }
+};
+
+int main()
+{
+    date s1("12/05/2020");
+    date s2(15, 8, 2021);
+
+    s1.show();
+    s2.show();
+
+    return 0;
+}
