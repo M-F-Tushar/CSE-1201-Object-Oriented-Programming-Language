a79bab3843544b1a03f02c3ff1d7058ec5497850
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-08-31 23:34:44 +0600

    Add files via upload

---
+++ OOP in C++/Leacture_05: Function Overloading/Code_Example/1.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+class coord {
+    int x, y;
+public:
+    coord()
+    {
+        x = 0;
+        y = 0;
+    }
+    coord(int i, int j)
+    {
+        x = i;
+        y = j;
+    }
+    void get_xy(int &i, int &j)
+    {
+        i = x;
+        j = y;
+    }
+    coord operator++()
+    {
+        x++;
+        y++;
+        return *this;
+    }
+};
+
+int main()
+{
+    coord o1(10,10);
+    int x, y;
+    
+    ++o1;
+    o1.get_xy(x,y);
+    cout << "x: " << x << ", y: " << y << endl;
+}
