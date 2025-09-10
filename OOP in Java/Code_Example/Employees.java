b06577d43a4e7306b276b2ebdc4dea367e05a49a
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-09-10 08:53:31 +0600

    Add files via upload

---
+++ OOP in Java/Code_Example/Employees.java
@@ -0,0 +1,46 @@
+package For_Final;
+
+class Employee 
+{
+    // Private fields (encapsulated data)
+    private int id;
+    private String name;
+
+    // Setter methods (to modify private fields)
+    public void setId(int id) 
+    {
+        this.id = id;
+    }
+
+    public void setName(String name) 
+    {
+        this.name = name;
+    }
+
+    // Getter methods (to access private fields)
+    public int getId() 
+    {
+        return id;
+    }
+
+    public String getName() 
+    {
+        return name;
+    }
+}
+
+// Main class to run the program
+public class Employees 
+{
+    public static void main(String[] args) {
+        Employee emp = new Employee();
+
+        // Using setters to assign values
+        emp.setId(101);
+        emp.setName("Geek");
+
+        // Using getters to retrieve values
+        System.out.println("Employee ID: " + emp.getId());
+        System.out.println("Employee Name: " + emp.getName());
+    }
+}
