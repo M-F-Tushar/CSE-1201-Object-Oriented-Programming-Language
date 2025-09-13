ee51a7e03cf99d7e50b10492cadf9066600e5b34
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-09-13 15:59:36 +0600

    Update VirtualDemo.java

--- OOP in Java/Code_Example/VirtualDemo.java
+++ OOP in Java/Code_Example/VirtualDemo.java
@@ -1,6 +1,7 @@
 package Polymorphism;
 
-public class VirtualDemo {
+public class VirtualDemo 
+{
     public static void main(String[] args) {
         Salary s = new Salary("Mohd Mohtashim", "Ambehta, UP", 3, 3600.00);
         Employee e = new Salary("John Adams", "Boston, MA", 2, 2400.00);
@@ -12,3 +13,4 @@ public class VirtualDemo {
         e.mailCheck();
     }
 }
+
