fc7a4534195a9f0c66647994392238c5b0be6636
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-09-13 16:00:38 +0600

    Update Salary.java

--- OOP in Java/Code_Example/Salary.java
+++ OOP in Java/Code_Example/Salary.java
@@ -1,6 +1,7 @@
 package Polymorphism;
 
-public class Salary extends Employee {
+public class Salary extends Employee 
+{
     private double salary;
 
     public Salary(String name, String address, int number, double salary) {
@@ -18,3 +19,4 @@ public class Salary extends Employee {
     public void setSalary(double newSalary) { if (newSalary >= 0.0) salary = newSalary; }
     public double computePay() { return salary / 52; }
 }
+
