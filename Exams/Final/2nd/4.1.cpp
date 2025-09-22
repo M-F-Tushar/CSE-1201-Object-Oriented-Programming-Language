0b7a48902a16559655efe6a9b0cb49d098da4242
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-09-22 07:45:43 +0600

    Add files via upload

---
+++ Exams/Final/2nd/4.1.cpp
@@ -0,0 +1,14 @@
+/*
+4 (a)
+STL (Standard Template Library):
+
+Ready-made library in C++ for data structures & algorithms.
+
+Components:
+
+Containers → store data (vector, list, map).
+
+Algorithms → sort, search, etc.
+
+Iterators → connect algorithms with containers.
+*/
