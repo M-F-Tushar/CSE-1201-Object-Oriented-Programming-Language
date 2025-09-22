f810b18844d1486f900fb58e92a8cb7f3e2f8a8e
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-09-22 07:45:16 +0600

    Add files via upload

---
+++ Exams/Final/2nd/3.2.cpp
@@ -0,0 +1,10 @@
+/*
+Q3 (b)
+Inheritance and Code Reusability:
+
+You don’t rewrite code.
+
+Common features are in the base class → reused in derived classes.
+
+Example: Employee base class → both Teacher and Clerk classes can use it.
+*/
