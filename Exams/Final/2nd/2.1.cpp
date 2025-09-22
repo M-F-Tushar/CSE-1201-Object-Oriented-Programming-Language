546de7477b57dcae9eea999d29eb22ec6840a616
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-09-22 07:44:21 +0600

    Add files via upload

---
+++ Exams/Final/2nd/2.1.cpp
@@ -0,0 +1,14 @@
+/*
+Q2 (a)
+Copy Constructor:
+
+A copy constructor initializes a new object from an existing object.
+
+It copies values of attributes.
+
+Syntax:
+
+ClassName (const ClassName &obj) {
+    // Copy values from obj to the new object
+}
+*/
