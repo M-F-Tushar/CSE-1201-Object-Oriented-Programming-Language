7884c2304dc235c23c8c6459a8257aae20fda061
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-09-22 07:45:12 +0600

    Add files via upload

---
+++ Exams/Final/2nd/3.1.cpp
@@ -0,0 +1,14 @@
+/*
+Q3 (a)
+Inheritance:
+
+Inheritance allows one class (child) to use properties and methods of another class (parent).
+
+Types:
+
+Public inheritance → public & protected of base stay same in derived.
+
+Protected inheritance → public & protected of base become protected in derived.
+
+Private inheritance → public & protected of base become private in derived.
+*/
