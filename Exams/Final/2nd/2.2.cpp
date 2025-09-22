4201829a062634c985b86dff76dd550b02ef887c
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-09-22 07:44:25 +0600

    Add files via upload

---
+++ Exams/Final/2nd/2.2.cpp
@@ -0,0 +1,10 @@
+/*
+Q2 (b)
+Constructor vs Destructor
+Feature	Constructor	        Destructor
+Purpose	Initialize object	Clean up (release memory/resources)
+Name	Same as class name	Same as class but with ~
+Parameters	Can take parameters	Cannot take parameters
+Overloading	Can be overloaded	Cannot be overloaded
+Call time	Called when object is created	Called when object is destroyed
+*/
