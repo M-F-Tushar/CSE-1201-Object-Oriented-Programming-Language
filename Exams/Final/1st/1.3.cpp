31fc27469fa2cbd75ac4d6c7c4a5860624c1caad
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-09-23 08:50:32 +0600

    Add files via upload

---
+++ Exams/Final/1st/1.3.cpp
@@ -0,0 +1,20 @@
+/*
+Q1 (c) Given class sample, declare object ob with a = 10 and c = 'X'. [10 marks]
+
+Class:
+
+class sample {
+  int a;
+  char c;
+public:
+  sample(int x, char ch) { a = x; c = ch; }
+};
+
+
+Declaration:
+
+sample ob(10, 'X');
+
+
+Explanation: The constructor sample(int, char) takes two parameters. When you write sample ob(10, 'X'); the constructor runs and sets a=10 and c='X'.
+*/
