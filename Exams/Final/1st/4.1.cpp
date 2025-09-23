06be1c3de2065ad92cd6ee49084f33b3efda8574
Mahir Faysal Tushar <www.mahirfaysaltushar@gmail.com>
2025-09-23 08:52:58 +0600

    Add files via upload

---
+++ Exams/Final/1st/4.1.cpp
@@ -0,0 +1,11 @@
+/*
+
+Q4 (a) What are containers, algorithms, and iterators in STL? [5 marks]
+
+Containers: Data structures that hold objects (e.g., vector, list, map).
+
+Algorithms: Generic functions that operate on ranges defined by iterators (sort, find, accumulate).
+
+Iterators: Objects that point to elements inside containers and allow traversal (like generalized pointers). Algorithms use iterators to be container-agnostic.
+
+*/
